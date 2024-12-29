70896e7fb25863330d84e81f6e69ddc465215fdf
Zheng Zhi Yuan <kevinjone25@g.ncu.edu.tw>
2024-12-30 01:25:27 +0800

    docs: add comment

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# socketPrograming
-A real-time chat room in web
+# socket client Programing
 
-Utilized Golang, sockets, multithreading, non-blocking and other related technologies. 
+client server web socket chat
 
-## Implementation details
-see: https://drive.google.com/file/d/1sAU3IVBe1-lvsLuSLKYj6UhAYsLzRoZB/view
--- client/src/main.go
+++ client/src/main.go
@@ -8,10 +8,13 @@ import (
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	// 前端網站
 	r.LoadHTMLFiles(
 		"./src/views/index.html",
 	)
+	// 當有人連進來時會呼叫 controller 裡面的 SendHTML
 	r.GET("/room", controllers.SendHTML)
+	// 只要有人連進來就會呼叫 controller 裡面的 ConnectWebSocket
 	r.GET("/room/ws", controllers.ConnectWebSocket)
 
 	return r
